faae7e6c7b1a89f369671a3eabe1e2671766674a
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2016-03-26 21:34:32 +0100

    added QtCreator project wizards

---
+++ qtcreator/wizards/sketch/file.ino
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+/**************************************
+ global variables
+ *************************************/
+
+
+/**************************************
+ functions
+ *************************************/
+
+
+// setup() is called once at startup
+void setup()
+{
+
+}
+
+// loop() is called periodically
+void loop()
+{
+
+}
